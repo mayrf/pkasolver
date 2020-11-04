94c501cb63b29520e8d3bb67c016aabda37d6ded
Fritz Mayr <fmayr@a7srv2.pch.univie.ac.at>
2020-11-04 21:25:02 +0100

    test commit

---
+++ pkasolver/util.py
@@ -0,0 +1,11 @@
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import PandasTools
+from rdkit.Chem import AllChem
+
+
+def sdfimport (file):
+    #import to pd-df with rdkit
+    #name columns 
+    #return pd
+
