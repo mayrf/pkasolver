8daeda77358fbf4ed3e525d69d2ace7c1bfacbc3
Fritz Mayr <fmayr@a7srv2.pch.univie.ac.at>
2021-10-12 11:02:06 +0200

    fixed calculation of number of node features from one hot encoding (a bit hacky)

--- examples/paper_run_1/config.py
+++ examples/paper_run_1/config.py
@@ -1,3 +1,5 @@
+from pkasolver.constants import node_feature_values
+
 # General
 
 # raw data paths
@@ -15,28 +17,29 @@ NUM_ESTIMATORS = 100  # 1000 Baltruschat
 # GCN
 BATCH_SIZE = 64
 LEARNING_RATE = 0.001
-NUM_EPOCHS = 4000  # 3000
+NUM_EPOCHS = 2000  # 3000
 
 NUM_GRAPH_LAYERS = 4
 NUM_LINEAR_LAYERS = 2
 HIDDEN_CHANNELS = 96
 
 list_node_features = [
-    # "atomic_number",
     "element_onehot",
     "formal_charge",
-    #     'chiral_tag',
     "hybridization",
     "total_num_Hs",
-    #     'explicit_num_Hs',
     "aromatic_tag",
     "total_valence",
     "total_degree",
     "is_in_ring",
-    #     'amide_center_atom'
     "reaction_center",
 ]
 list_edge_features = ["bond_type", "is_conjugated", "rotatable"]
 
-num_node_features = len(list_node_features)
+i = 0
+for feat in list_node_features:
+    i += len(node_feature_values[feat])
+num_node_features = i
+
+# num_node_features = len(list_node_features)
 num_edge_features = len(list_edge_features)
