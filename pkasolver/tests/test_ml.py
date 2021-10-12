aa4ee47f7924da11b372fff341d696f0d78e01b7
wiederm <marcus.wieder@univie.ac.at>
2021-10-12 14:22:46 +0200

    calculate features/fix new tests

--- pkasolver/tests/test_ml.py
+++ pkasolver/tests/test_ml.py
@@ -17,7 +17,12 @@ from pkasolver.ml_architecture import (
     gcn_full_training,
 )
 import torch
-from pkasolver.constants import DEVICE, node_feature_values, edge_feature_values
+from pkasolver.constants import (
+    DEVICE,
+    node_feature_values,
+    edge_feature_values,
+    calculate_nr_of_features,
+)
 
 models = [
     ("GCNPairSingleConv", GCNPairSingleConv),
@@ -104,11 +109,9 @@ def test_train_gcn_models():
     dataset = make_pyg_dataset_from_dataframe(df, list_n, list_e, paired=True)
     dataloader = dataset_to_dataloader(dataset, batch_size=64, shuffle=False)
 
-    # calculate node features
-    i_n = 0
-    for feat in list_n:
-        i_n += len(node_feature_values[feat])
-    num_node_features = i_n
+    # calculate node/edge features
+    num_node_features = calculate_nr_of_features(list_n)
+    num_edge_features = calculate_nr_of_features(list_e)
 
     i_e = 0
     for feat in list_e:
@@ -163,13 +166,16 @@ def test_train_new_models():
     df = preprocess(sdf_filepaths["Novartis"])
 
     # number of node/edge features
-    list_n = ["atomic_number", "formal_charge"]
+    list_n = ["element", "formal_charge"]
     list_e = ["bond_type", "is_conjugated"]
     # start with generating datasets based on charge
 
     # generated PairedData set
     dataset = make_pyg_dataset_from_dataframe(df, list_n, list_e, paired=True)
     dataloader = dataset_to_dataloader(dataset, batch_size=64, shuffle=False)
+    # calculate node/edge features
+    num_node_features = calculate_nr_of_features(list_n)
+    num_edge_features = calculate_nr_of_features(list_e)
 
     for model_name, model_class in new_models:
         print(model_name)
@@ -179,8 +185,8 @@ def test_train_new_models():
         for attention_mode in [False, True]:
             print(attention_mode)
             model = model_class(
-                num_node_features=len(list_n),
-                num_edge_features=len(list_e),
+                num_node_features=num_node_features,
+                num_edge_features=num_edge_features,
                 attention=attention_mode,
             ).to(device=DEVICE)
             print(model)
