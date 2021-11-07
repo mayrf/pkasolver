e5f650ffa2a9281ddf0ae12eb110803d4f630506
wiederm <marcus.wieder@univie.ac.at>
2021-11-07 20:49:51 +0100

    some comments and improvment for split script

--- scripts/split_epik_output_sdf.py
+++ scripts/split_epik_output_sdf.py
@@ -2,37 +2,48 @@ from rdkit import Chem
 import copy
 import gzip
 
+version = 0
 data_dir = "/data/shared/projects/pkasolver-data/"
 schroedinger_dir = "/data/shared/software/schrodinger2021-1/"
-# epik = schroedinger_dir + "epik"
-# convert = schroedinger_dir + "utilities/structconvert"
 
-input_file = f"{data_dir}chembl_epik.sdf"
-suppl = Chem.SDMolSupplier(str(input_file), removeHs=False)
-w = Chem.SDWriter(f"{data_dir}chembl_epik_split.sdf")
-a = 0
-for mol in suppl:
+input_file = gzip.open(f"{data_dir}/mols_chembl_with_pka_for_v{version}.sdf.gz", "rb")
+output_file = gzip.open(
+    f"{data_dir}/split_mols_chembl_with_pka_for_v{version}.sdf.gz", "wt+"
+)
+
+reader = Chem.ForwardSDMolSupplier(input_file, removeHs=False)
+writer = Chem.SDWriter(output_file)
+
+
+for idx, mol in enumerate(reader):
+
     try:
         props = mol.GetPropsAsDict()
-
-        l = len([s for s in props.keys() if "r_epik_pKa" in s])
-
-        for prop in props.keys():
-            mol.ClearProp(prop)
-        for i in range(l):
-            mol_copy = copy.deepcopy(mol)
-            mol_copy.SetProp(f"ID", props[f"chembl_id"])
-            mol_copy.SetProp(f"pKa", props[f"r_epik_pKa_{i+1}"])
-            mol_copy.SetProp(f"marvin_pKa", props[f"r_epik_pKa_{i+1}"])
-            mol_copy.SetProp(f"marvin_atom", str(props[f"i_epik_pKa_atom_{i+1}"] - 1))
-            mol_copy.SetProp(f"pka_number", str(i + 1))
-            w.write(mol_copy)
-            # print(props[f"r_epik_pKa_{i+1}"])
-    except Exception as e:
-        print(f"Error at molecule number {a}")
+    except AttributeError as e:
         print(e)
-    a += 1
-    # if a > 1000:
-    #     break
-w.close()
-print(f"finished splitting {a} molecules")
+        continue
+
+    if not any("r_epik_pKa" in string for string in props.keys()):
+        # skip if no pka value was calculated
+        print(f"No pKa set for mol {idx} ...")
+        continue
+
+    # calculate number of pka values set
+    number_of_pka_values = len([s for s in props.keys() if "r_epik_pKa" in s])
+
+    # clear poperty entries
+    for prop in props.keys():
+        mol.ClearProp(prop)
+
+    # for each calculated pka value
+    for i in range(number_of_pka_values):
+        mol_copy = copy.deepcopy(mol)
+        mol_copy.SetProp(f"ID", props[f"chembl_id"])
+        mol_copy.SetProp(f"pKa", props[f"r_epik_pKa_{i+1}"])
+        mol_copy.SetProp(f"marvin_pKa", props[f"r_epik_pKa_{i+1}"])
+        mol_copy.SetProp(f"marvin_atom", str(props[f"i_epik_pKa_atom_{i+1}"] - 1))
+        mol_copy.SetProp(f"pka_number", str(i + 1))
+        writer.write(mol_copy)
+
+writer.close()
+print(f"finished splitting {idx} molecules")
