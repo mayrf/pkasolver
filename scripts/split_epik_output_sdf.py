470ba02fb7ea0da1986ebb219c9b9dc89671482e
wiederm <marcus.wieder@univie.ac.at>
2021-11-15 19:20:49 +0100

    adding some comments, renaming some variables

--- scripts/split_epik_output_sdf.py
+++ scripts/split_epik_output_sdf.py
@@ -5,59 +5,81 @@ from pkasolver.chem import create_conjugate
 
 version = 0
 data_dir = "/data/shared/projects/pkasolver-data/"
-schroedinger_dir = "/data/shared/software/schrodinger2021-1/"
 input_file = f"{data_dir}chembl_epik.sdf"
+
 suppl = Chem.SDMolSupplier(str(input_file), removeHs=True)
-w = Chem.SDWriter(f"{data_dir}chembl_epik_split.sdf")
-a = 0
 
-for mol in suppl:
-    try:
-        props = mol.GetPropsAsDict()
-        l = len([s for s in props.keys() if "r_epik_pKa" in s])
-        atoms, pkas = [], []
-        for i in range(l):
-            pkas.append(
-                (
-                    float(props[f"r_epik_pKa_{i+1}"]),
-                    int(props[f"i_epik_pKa_atom_{i+1}"]) - 1,
-                    props[f"chembl_id"],
-                )
+with Chem.SDWriter(f"{data_dir}/chembl_epik_split_v{version}.sdf") as writer:
+    for nr_of_mols, mol in enumerate(suppl):
+        try:
+            props = mol.GetPropsAsDict()
+            nr_of_protonation_states = len(
+                [s for s in props.keys() if "r_epik_pKa" in s]
             )
-        acids = [pka for pka in pkas if pka[0] < 7]
-        bases = pkas[len(acids) :]
-        # print(f"{props[f'chembl_id']} acids: {acids}")
-        # print(f"{props[f'chembl_id']} bases: {bases}")
-        for prop in props.keys():
-            mol.ClearProp(prop)
-        acid_mols = [mol]
-        for i, acid in enumerate(acids[::-1]):
-            new_mol = create_conjugate(acid_mols[i], acid[1], acid[0], pH=7)
-            new_mol.SetProp(f"ID", str(acid[2]))
-            new_mol.SetProp(f"pKa", str(acid[0]))
-            new_mol.SetProp(f"marvin_pKa", str(acid[0]))
-            new_mol.SetProp(f"marvin_atom", str(acid[1]))
-            new_mol.SetProp(f"pka_number", f"acid_{i + 1}")
-            acid_mols.append(new_mol)
-        base_mols = [mol]
-        for i, base in enumerate(bases):
-            new_mol = create_conjugate(base_mols[i], base[1], base[0], pH=7)
-            new_mol.SetProp(f"ID", str(base[2]))
-            new_mol.SetProp(f"pKa", str(base[0]))
-            new_mol.SetProp(f"marvin_pKa", str(base[0]))
-            new_mol.SetProp(f"marvin_atom", str(base[1]))
-            new_mol.SetProp(f"pka_number", f"base_{i + 1}")
-            base_mols.append(new_mol)
-        mols = acid_mols[1:] + base_mols[1:]
-        # print(len(mols))
-        for mol in mols:
-            w.write(mol)
+            pkas = []
+            for i in range(nr_of_protonation_states):
+                pkas.append(
+                    (
+                        float(props[f"r_epik_pKa_{i+1}"]),
+                        int(props[f"i_epik_pKa_atom_{i+1}"]) - 1,
+                        props[f"chembl_id"],
+                    )
+                )
 
-    except Exception as e:
-        print(f"Error at molecule number {a}")
-        print(e)
-    a += 1
-    if a > 1000:
-        break
-w.close()
-print(f"finished splitting {a} molecules")
+            # calculate number of acidic and basic pka values
+            nr_of_acids = sum(pka[0] <= 7 for pka in pkas)
+            nr_of_bases = sum(pka[0] > 7 for pka in pkas)
+            assert nr_of_acids + nr_of_bases == len(pkas)
+
+            acidic_mols_properties = [mol for mol in pkas if mol[0] <= 7]
+            basic_mols_properties = [mol for mol in pkas if mol[0] > 7]
+
+            assert len(acidic_mols_properties) == nr_of_acids
+            assert len(basic_mols_properties) == nr_of_bases
+
+            # clear porps
+            for prop in props.keys():
+                mol.ClearProp(prop)
+
+            # add neutral mol as first acidic mol
+            acidic_mols = [mol]
+            for idx, acid_prop in enumerate(acidic_mols_properties):
+                new_mol = create_conjugate(
+                    acidic_mols[-1], acid_prop[1], acid_prop[0], pH=7
+                )
+                new_mol.SetProp(f"ID", str(acid_prop[2]))
+                new_mol.SetProp(f"pKa", str(acid_prop[0]))
+                new_mol.SetProp(f"marvin_pKa", str(acid_prop[0]))
+                new_mol.SetProp(f"marvin_atom", str(acid_prop[1]))
+                new_mol.SetProp(f"pka_number", f"acid_{idx + 1}")
+                # add current mol to list of acidic mol. for next
+                # lower pKa value, this mol is starting structure
+                acidic_mols.append(new_mol)
+            # same workflow for basic mols
+            basic_mols = [mol]
+            for idx, basic_prop in enumerate(basic_mols_properties):
+                new_mol = create_conjugate(
+                    basic_mols[-1], basic_prop[1], basic_prop[0], pH=7
+                )
+                new_mol.SetProp(f"ID", str(basic_prop[2]))
+                new_mol.SetProp(f"pKa", str(basic_prop[0]))
+                new_mol.SetProp(f"marvin_pKa", str(basic_prop[0]))
+                new_mol.SetProp(f"marvin_atom", str(basic_prop[1]))
+                new_mol.SetProp(f"pka_number", f"base_{idx + 1}")
+                basic_mols.append(new_mol)
+
+            # combine basic and acidic mols, skipp neutral mol
+            mols = acidic_mols[1:] + basic_mols[1:]
+            assert len(mols) == len(acidic_mols_properties) + len(basic_mols_properties)
+
+            for mol in mols:
+                writer.write(mol)
+
+        except Exception as e:
+            print(f"Error at molecule number {nr_of_mols}")
+            print(e)
+
+        if nr_of_mols > 500:
+            break
+
+print(f"finished splitting {nr_of_mols} molecules")
