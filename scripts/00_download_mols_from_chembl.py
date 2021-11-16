5f0ecaa2d0e3789b4a5035f75cc7d40aaa8a63c1
wiederm <marcus.wieder@univie.ac.at>
2021-11-16 20:18:56 +0100

    changing namings to indicate ordering; encapsuling in main

---
+++ scripts/00_download_mols_from_chembl.py
@@ -0,0 +1,29 @@
+from chembl_webresource_client.new_client import new_client
+from tqdm import tqdm
+import gzip
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output", help="output filename")
+    args = parser.parse_args()
+
+    print("outputfile:", args.output)
+
+    molecule = new_client.molecule
+    # mols = molecule.filter(max_phase=4)
+    mols = molecule.filter(molecule_type="Small molecule").filter(
+        molecule_properties__mw_freebase__gte=70
+    )
+    print(len(mols))
+
+    with gzip.open(args.output, "wb+") as output:
+        for mol in tqdm(mols):
+            if mol["molecule_structures"]:
+                output.write(mol["molecule_structures"]["molfile"].encode())
+                output.write(b"$$$$\n")
+
+
+if __name__ == "__main__":
+    main()
