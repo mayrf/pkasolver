2d7e727748ad359fcb978209ee1c7f633afad96e
wiederm <marcus.wieder@univie.ac.at>
2021-11-18 15:54:21 +0100

    adding tqdm, capturing assert fail

--- scripts/05_data_preprocess.py
+++ scripts/05_data_preprocess.py
@@ -1,11 +1,13 @@
 import argparse
 import gzip
 import pickle
-from rdkit import Chem
-from pkasolver.chem import create_conjugate
-from pkasolver.data import make_features_dicts, mol_to_paired_mol_data
-from pkasolver.constants import NODE_FEATURES, EDGE_FEATURES
+
 import torch
+import tqdm
+from pkasolver.chem import create_conjugate
+from pkasolver.constants import EDGE_FEATURES, NODE_FEATURES
+from pkasolver.data import make_features_dicts, mol_to_paired_mol_data
+from rdkit import Chem
 from rdkit.Chem.AllChem import Compute2DCoords
 
 # define selection of node and edge features
@@ -58,10 +60,7 @@ def main():
 def processing(suppl, args):
     pair_data_list = []
     print("Start processing data...")
-    for i, mol in enumerate(suppl):
-        if i > 5000:
-            break
-        # Compute2DCoords(mol)
+    for i, mol in tqdm.tqdm(enumerate(suppl)):
         props = mol.GetPropsAsDict()
         try:
             pka = props["pKa"]
@@ -69,7 +68,12 @@ def processing(suppl, args):
             print(f"No pka found for {i}. molecule: {props}")
             continue
         atom = props["marvin_atom"]
-        conj = create_conjugate(mol, atom, pka)
+        try:
+            conj = create_conjugate(mol, atom, pka)
+        except AssertionError as e:
+            print(f"mol {i} is failing because: {e}")
+            continue
+
         # sort mol and conj into protonated and deprotonated molecule
         if int(mol.GetAtomWithIdx(atom).GetFormalCharge()) > int(
             conj.GetAtomWithIdx(atom).GetFormalCharge()
@@ -81,20 +85,12 @@ def processing(suppl, args):
             deprot = mol
         # create PairData object from prot and deprot with the selected node and edge features
         m = mol_to_paired_mol_data(
-            prot,
-            deprot,
-            atom,
-            selected_node_features,
-            selected_edge_features,
+            prot, deprot, atom, selected_node_features, selected_edge_features,
         )
         m.y = torch.tensor(pka, dtype=torch.float32)
         m.pka_type = props["pka_number"]
         m.ID = props["ID"]
         pair_data_list.append(m)
-
-        if i % 1000 == 0:
-            if i != 0:
-                print(f"{i} molecules converted")
 
     with open(args.output, "wb") as f:
         pickle.dump(pair_data_list, f)
