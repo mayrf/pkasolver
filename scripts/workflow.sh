d6b8c0688da6dcb6601251653faf4452f7864477
wiederm <marcus.wieder@univie.ac.at>
2021-11-20 10:11:11 +0100

    adding global data_path variable

--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/bash
 
 version=0
+data_path='/data/shared/projects/pkasolver-data/'
 
 python 00_downdload_mols_from_chembl.py --output /data/local/00_mols_chembl_v${version}.sdf.gz
-python 01_convert_sdf_to_mae.py --input /data/local/00_mols_chembl_v${version}.sdf.gz --output /data/shared/projects/pkasolver-data/01_mols_chembl_v${version}.mae.gz
-python 02_predict_pka_with_epik.py --input /data/shared/projects/pkasolver-data/01_mols_chembl_v${version}.mae.gz --output /data/shared/projects/pkasolver-data/02_mols_chembl_with_pka_v${version}.mae.gz
-python 03_convert_mae_to_sdf.py --input /data/shared/projects/pkasolver-data/02_mols_chembl_with_pka_v${version}.mae.gz --output /data/shared/projects/pkasolver-data/03_mols_chembl_with_pka_v${version}.sdf.gz
-python 04_split_epik_output.py --input /data/shared/projects/pkasolver-data/03_mols_chembl_with_pka_v${version}.sdf.gz --output /data/shared/projects/pkasolver-data/04_split_mols_chembl_with_pka_v${version}.sdf
+python 01_convert_sdf_to_mae.py --input /data/local/00_mols_chembl_v${version}.sdf.gz --output {data_path}/01_mols_chembl_v${version}.mae.gz
+python 02_predict_pka_with_epik.py --input {data_path}/01_mols_chembl_v${version}.mae.gz --output {data_path}/02_mols_chembl_with_pka_v${version}.mae.gz
+python 03_convert_mae_to_sdf.py --input {data_path}/02_mols_chembl_with_pka_v${version}.mae.gz --output {data_path}/03_mols_chembl_with_pka_v${version}.sdf.gz
+python 04_split_epik_output.py --input {data_path}/03_mols_chembl_with_pka_v${version}.sdf.gz --output {data_path}/04_split_mols_chembl_with_pka_v${version}.sdf
+python 05_data_preprocess.py --input ${data_path}/04_split_mols_v1.sdf --output ${data_path}/05_chembl_pretrain_data_v${version}.pkl
 
