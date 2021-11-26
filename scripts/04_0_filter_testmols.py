1fb35f2cd6eb19ccdb64ba6ece5b67c4aa5e012c
wiederm <marcus.wieder@univie.ac.at>
2021-11-26 13:47:25 +0100

    change order of call, also write to gzip and neutralize mols

--- scripts/04_0_filter_testmols.py
+++ scripts/04_0_filter_testmols.py
@@ -3,6 +3,7 @@ import gzip
 from rdkit import Chem
 import tqdm
 from rdkit import RDLogger
+from rdkit.Chem.MolStandardize import rdMolStandardize
 
 
 def main():
@@ -29,17 +30,17 @@ def main():
         if input_zipped:
             with gzip.open(i, "r") as fh:
                 suppl = Chem.ForwardSDMolSupplier(fh, removeHs=False)
-                ini_list.extend(ini_filter(suppl, args))
+                ini_list.extend(ini_filter(suppl))
             with gzip.open(i, "r") as fh:
                 suppl = Chem.ForwardSDMolSupplier(fh, removeHs=False)
-                smi_list.extend(smi_filter(suppl, args))
+                smi_list.extend(smi_filter(suppl))
         else:
             with open(i, "rb") as fh:
                 suppl = Chem.ForwardSDMolSupplier(fh, removeHs=False)
-                ini_list.extend(ini_filter(suppl, args))
+                ini_list.extend(ini_filter(suppl))
             with open(i, "rb") as fh:
                 suppl = Chem.ForwardSDMolSupplier(fh, removeHs=False)
-                smi_list.extend(smi_filter(suppl, args))
+                smi_list.extend(smi_filter(suppl))
 
     print(f"{len(ini_list)} inchi test molecules found")
     print(f"{len(smi_list)} smi test molecules found")
@@ -61,39 +62,42 @@ def main():
             processing(suppl, args, ini_list, smi_list)
 
 
-def smi_filter(suppl, args):
+def smi_filter(suppl):
+    un = rdMolStandardize.Uncharger()
     smi_list = []
     for mol in suppl:
+        un.uncharge(mol)
         smi_list.append(Chem.MolToSmiles(mol))
-    # print(smi_list)
     return smi_list
 
 
-def ini_filter(suppl, args):
+def ini_filter(suppl):
+    un = rdMolStandardize.Uncharger()
     ini_list = []
     for mol in suppl:
+        un.uncharge(mol)
         ini_list.append(Chem.inchi.MolToInchi(mol))
     return ini_list
 
 
 def processing(suppl, args, ini_list, smi_list):
     dup = 0
-    with Chem.SDWriter(args.output) as writer:
+    un = rdMolStandardize.Uncharger()
+    with Chem.SDWriter(gzip.open(args.output, "wt+")) as writer:
+        skipped = 0
         for mol in tqdm.tqdm(suppl):
-
-            if Chem.inchi.MolToInchi(mol) in ini_list:
-                if Chem.MolToSmiles(mol) in smi_list:
-                    print(" inchi and smiles duplicate found!")
+            if mol:
+                un.uncharge(mol)
+                inchi = Chem.inchi.MolToInchi(mol)
+                smiles = Chem.MolToSmiles(mol)
+                if inchi in ini_list or smiles in smi_list:
+                    dup += 1
                 else:
-                    print(" inchi duplicate found!")
-                dup += 1
-            elif Chem.MolToSmiles(mol) in smi_list:
-                print("smiles duplicate found!")
-                dup += 1
+                    writer.write(mol)
             else:
-                writer.write(mol)
+                skipped += 1
     print(f"{dup} duplicate molecules found and discarted")
-    # pass
+    print(f"{skipped} molecules skipped")
 
 
 if __name__ == "__main__":
