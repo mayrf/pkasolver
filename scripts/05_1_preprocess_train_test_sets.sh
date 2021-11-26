8af2be4e8d0b356e5eea5b231080de9ba8edc605
Fritz Mayr <fmayr@a7srv2.pch.univie.ac.at>
2021-11-26 11:25:47 +0100

    script for filtering pretraining for test sets added

--- scripts/05_1_preprocess_train_test_sets.sh
+++ scripts/05_1_preprocess_train_test_sets.sh
@@ -6,4 +6,4 @@ data_path=/data/shared/projects/pkasolver-data
 
 python 05_data_preprocess.py --input ${data_path}/04_novartis_cleaned_mono_unique_notraindata.sdf --output ${data_path}/05_novartis_cleaned_mono_unique_notraindata.pkl 
 python 05_data_preprocess.py --input ${data_path}/04_AvLiLuMoVe_cleaned_mono_unique_notraindata.sdf --output ${data_path}/05_AvLiLuMoVe_cleaned_mono_unique_notraindata.pkl
-python 05_data_preprocess.py --input ${data_path}/04_combined_training_datasets_unique.sdf --output ${data_path}/05_combined_training_datasets_unique.pkl
+python 05_data_preprocess.py --input ${data_path}/04_combined_training_datasets_unique.sdf --output ${data_path}/05_combined_training_datasets_unique.pklsdf
--- scripts/workflow.sh
+++ scripts/workflow.sh
@@ -8,7 +8,8 @@ python 01_convert_sdf_to_mae.py --input /data/local/00_mols_chembl_v${version}.s
 python 02_predict_pka_with_epik.py --input {data_path}/01_mols_chembl_v${version}.mae.gz --output {data_path}/02_mols_chembl_with_pka_v${version}.mae.gz
 python 03_convert_mae_to_sdf.py --input {data_path}/02_mols_chembl_with_pka_v${version}.mae.gz --output {data_path}/03_mols_chembl_with_pka_v${version}.sdf.gz
 python 04_split_epik_output.py --input {data_path}/03_mols_chembl_with_pka_v${version}.sdf.gz --output {data_path}/04_split_mols_chembl_with_pka_v${version}.sdf
-python 05_data_preprocess.py --input ${data_path}/04_split_mols_v1.sdf --output ${data_path}/05_chembl_pretrain_data_v${version}.pkl
+python 04_1_filter_testmols.py --input ${data_path}/04_split_mols_chembl_with_pka_v${version}.sdf.gz  --output ${data_path}/04_split_mols_v${version}.sdf --filter ${data_path}/00_AvLiLuMoVe_testdata.sdf,${data_path}/00_novartis_testdata.sdf
+python 05_data_preprocess.py --input ${data_path}/04_split_mols_v${version}.sdf --output ${data_path}/05_chembl_pretrain_data_v${version}.pkl
 # the following calls need some rework
 # NOTE: we have this in the repository and should use the files from there
 python 05_data_preprocess.py --input ${data_path}/04_novartis_cleaned_mono_unique_notraindata.sdf --output ${data_path}/05_novartis_cleaned_mono_unique_notraindata.pkl 
