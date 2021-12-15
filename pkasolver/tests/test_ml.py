792a63f8d91d2832f525fb9bde7c9b0ecc322f83
wiederm <marcus.wieder@univie.ac.at>
2021-12-15 08:23:33 +0100

    also test GINPairV3

--- pkasolver/tests/test_ml.py
+++ pkasolver/tests/test_ml.py
@@ -9,7 +9,7 @@ from pkasolver.ml_architecture import (
     GINProt,
     GINPairV1,
     GINPairV2,
-    AttentiveProt,
+    GINPairV3,
     GATProt,
     GATPair,
     AttentivePair,
@@ -141,9 +141,6 @@ new_models = [
     ("GINPairV2", GINPairV2),
     ("GATProt", GATProt),
     ("GATPair", GATPair),
-    ("AttentiveProt", AttentiveProt),
-    ("AttentivePair", AttentivePair),
-    ("AttentivePairV1", AttentivePairV1),
 ]
 
 
@@ -202,3 +199,138 @@ def test_train_new_models():
 
             gcn_train(model, dataloader, optimizer)
             gcn_test(model, dataloader)
+
+
+new_models = [
+    ("GINPairV1", GINPairV1),
+    ("GINPairV3", GINPairV3),
+]
+
+
+def test_only_GINPairV1_and_GINPairV3_models():
+    from pkasolver.ml_architecture import gcn_train, gcn_test
+    from pkasolver.data import (
+        load_data,
+        preprocess,
+        make_pyg_dataset_from_dataframe,
+    )
+    from pkasolver.ml import dataset_to_dataloader
+
+    sdf_filepaths = load_data()
+    df = preprocess(sdf_filepaths["Novartis"])
+
+    # number of node/edge features
+    list_n = [
+        "element",
+        "formal_charge",
+        "hybridization",
+        "total_num_Hs",
+        "aromatic_tag",
+        "total_valence",
+        "total_degree",
+        "is_in_ring",
+        "reaction_center",
+        "smarts",
+    ]
+    list_e = [
+        "bond_type",
+        "is_conjugated",
+        "rotatable",
+    ]  # start with generating datasets based on charge
+
+    # generated PairedData set
+    dataset = make_pyg_dataset_from_dataframe(df, list_n, list_e, paired=True)
+    dataloader = dataset_to_dataloader(dataset, batch_size=64, shuffle=False)
+    # calculate node/edge features
+    num_node_features = calculate_nr_of_features(list_n)
+    num_edge_features = calculate_nr_of_features(list_e)
+
+    for model_name, model_class in new_models:
+        print(model_name)
+        print(model_class)
+        #################
+        # test single models
+
+        model = model_class(
+            num_node_features=num_node_features,
+            num_edge_features=num_edge_features,
+            hidden_channels=96,
+            attention=False,
+        ).to(device=DEVICE)
+        print(model)
+        optimizer = torch.optim.AdamW(model.parameters(), lr=0.01)
+
+        gcn_train(model, dataloader, optimizer)
+        gcn_test(model, dataloader)
+        print("####################")
+        print(list(model.named_modules()))
+        print("####################")
+        print(model.get_submodule("GIN_d"))
+        print("####################")
+        print(model.get_submodule("lins.2"))
+        print("####################")
+        print(model.get_submodule("final_lin"))
+        print("####################")
+
+        print(
+            "Number of parameters: ",
+            sum(p.numel() for p in model.parameters() if p.requires_grad == True),
+        )
+
+
+def test_count_nr_of_parameters():
+
+    import torch.nn.functional as F
+    from torch.nn import Linear, ModuleList
+
+    lins = []
+    lins.append(Linear(64, 96))
+    for _ in range(0, 2):
+        lins.append(Linear(96, 96))
+    lins.append(Linear(96, 1))
+    model = ModuleList(lins)
+    print(model)
+    nr_of_parameters = sum(
+        p.numel() for p in model.parameters() if p.requires_grad == True
+    )
+    print(f"Number of parameters: {nr_of_parameters=}")
+
+
+def test_count_nr_of_parameters_for_GIN():
+
+    import torch.nn.functional as F
+    from torch_geometric.nn.models import GAT, GIN, AttentiveFP
+
+    list_n = [
+        "element",
+        "formal_charge",
+        "hybridization",
+        "total_num_Hs",
+        "aromatic_tag",
+        "total_valence",
+        "total_degree",
+        "is_in_ring",
+        "reaction_center",
+        "smarts",
+    ]
+    list_e = [
+        "bond_type",
+        "is_conjugated",
+        "rotatable",
+    ]  # start with generating datasets based on charge
+
+    # calculate node/edge features
+    num_node_features = calculate_nr_of_features(list_n)
+
+    model = GIN(
+        in_channels=num_node_features,
+        out_channels=32,
+        hidden_channels=96,
+        num_layers=4,
+        dropout=0.5,
+    )
+
+    nr_of_parameters = sum(
+        p.numel() for p in model.parameters() if p.requires_grad == True
+    )
+    print(f"Number of parameters: {nr_of_parameters=}")
