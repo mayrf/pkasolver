d4683714f7283102b4775d5d3d508e59c829875b
wiederm <marcus.wieder@univie.ac.at>
2022-01-08 23:23:45 +0100

    removing unused functions

--- pkasolver/chem.py
+++ pkasolver/chem.py
@@ -55,31 +55,32 @@ def create_conjugate(
     return mol_changed
 
 
-def generate_morgan_fp_array(df, mol_column, nBits=4096, radius=3, useFeatures=True):
-    """Take Pandas DataFrame, create Morgan fingerprints from the molecules in "mol_column"
-    and return them as numpy array.
-    """
-    length = len(df)
-    i = 0
-    for mol in df[mol_column]:
+# def generate_morgan_fp_array(df, mol_column, nBits=4096, radius=3, useFeatures=True):
+#     """Take Pandas DataFrame, create Morgan fingerprints from the molecules in "mol_column"
+#     and return them as numpy array.
+#     """
+#     length = len(df)
+#     i = 0
+#     for mol in df[mol_column]:
 
-        fp = np.array(
-            GetMorganFingerprintAsBitVect(
-                mol, radius=3, nBits=4096, useFeatures=useFeatures
-            )
-        )
-        if i == 0:
-            array = fp
-        else:
-            array = np.vstack((array, fp))
-        if i % 1000 == 0:
-            print(f"fp:{i} of {length}")
-        i += 1
-    return array
+#         fp = np.array(
+#             GetMorganFingerprintAsBitVect(
+#                 mol, radius=3, nBits=4096, useFeatures=useFeatures
+#             )
+#         )
+#         if i == 0:
+#             array = fp
+#         else:
+#             array = np.vstack((array, fp))
+#         if i % 1000 == 0:
+#             print(f"fp:{i} of {length}")
+#         i += 1
+#     return array
 
 
-# return whether a bond matches a particular smarts query
 def bond_smarts_query(bond, smarts):
+    # return whether a bond matches a particular smarts query
+
     for match in bond.GetOwningMol().GetSubstructMatches(Chem.MolFromSmarts(smarts)):
         if set((bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())) == set(match):
             return True
@@ -104,7 +105,7 @@ def make_smarts_features(atom, smarts_dict):
     return bits
 
 
-def calculate_tanimoto_coefficient(fp1, fp2):
-    set1 = set(fp1.nonzero()[0].tolist())
-    set2 = set(fp2.nonzero()[0].tolist())
-    return len(set1 & set2) / len(set1 | set2)
+# def calculate_tanimoto_coefficient(fp1, fp2):
+#     set1 = set(fp1.nonzero()[0].tolist())
+#     set2 = set(fp2.nonzero()[0].tolist())
+#     return len(set1 & set2) / len(set1 | set2)
