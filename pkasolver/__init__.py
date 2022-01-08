02b4497ffcc0fc29c19ca7891ee4eb7d1a26abb8
wiederm <marcus.wieder@univie.ac.at>
2022-01-08 23:23:31 +0100

    adding logger

--- pkasolver/__init__.py
+++ pkasolver/__init__.py
@@ -8,7 +8,15 @@ from .pkasolver import *
 
 # Handle versioneer
 from ._version import get_versions
+
 versions = get_versions()
-__version__ = versions['version']
-__git_revision__ = versions['full-revisionid']
+__version__ = versions["version"]
+__git_revision__ = versions["full-revisionid"]
 del get_versions, versions
+
+import logging
+
+# format logging message
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)1s()] %(message)s"
+# set logging level
+logging.basicConfig(format=FORMAT, datefmt="%d-%m-%Y:%H:%M", level=logging.INFO)
