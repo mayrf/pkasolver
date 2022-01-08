861ff936ba9371d597ef61804d46d444f5116243
wiederm <marcus.wieder@univie.ac.at>
2022-01-08 23:24:18 +0100

    remove unused functions

--- pkasolver/ml.py
+++ pkasolver/ml.py
@@ -2,9 +2,6 @@ from torch_geometric.loader import DataLoader
 import numpy as np
 import pandas as pd
 from pkasolver.constants import DEVICE
-from sklearn.metrics import r2_score
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
 
 
 # PyG Dataset to Dataloader
@@ -17,14 +14,6 @@ def dataset_to_dataloader(data, batch_size, shuffle=True):
     return DataLoader(
         data, batch_size=batch_size, shuffle=shuffle, follow_batch=["x_p", "x_d"]
     )
-
-
-def test_ml_model(baseline_models, X_data, y_data, dataset_name):
-    res = {"Dataset": dataset_name, "pKa_true": y_data}
-    for name, models in baseline_models.items():
-        for mode, model in models.items():
-            res[f"{name.upper()}_{mode}"] = model.predict(X_data[mode]).flatten()
-    return pd.DataFrame(res)
 
 
 def calculate_performance_of_model_on_data(model, loader):
@@ -51,7 +40,7 @@ def calculate_performance_of_model_on_data(model, loader):
     return np.array(x_dataset), np.array(y_dataset)
 
 
-def predict(model, loader):
+def predict_pka_value(model, loader):
     model.eval()
     results = []
     for data in loader:  # Iterate in batches over the training dataset.
@@ -71,56 +60,3 @@ def predict(model, loader):
         results.extend(y_pred.tolist())
 
     return np.array(results)
-
-
-def calculate_performance_of_model_on_data_old(model, loader):
-    model.eval()
-    y_dataset, x_dataset = [], []
-    for data in loader:  # Iterate in batches over the training dataset.
-
-        data.to(device=DEVICE)
-        y_pred = (
-            model(
-                x_p=data.x_p,
-                x_d=data.x_d,
-                edge_attr_p=data.edge_attr_p,
-                edge_attr_d=data.edge_attr_d,
-                data=data,
-            )
-            .reshape(-1)
-            .detach()
-        )
-
-        y_dataset.extend(y_pred.tolist())
-        x_dataset.extend(data.y.tolist())
-
-    return np.array(x_dataset), np.array(y_dataset)
-
-
-def test_graph_model(graph_models, loader, dataset_name):
-    res = {
-        "Dataset": dataset_name,
-    }
-
-    for model_name in graph_models:
-        model = graph_models[model_name]
-        model.to(device=DEVICE)
-        x, y = calculate_performance_of_model_on_data(model, loader)
-        res["pKa_true"], res[f"{model_name}"] = x, y
-        MAE = mean_absolute_error(x, y)
-        RMSE = np.sqrt(mean_squared_error(x, y))
-        # print(f"{dataset_name} - {model_name}: MAE {MAE}, RMSE {RMSE}")
-    return pd.DataFrame(res)
-
-
-def calc_testset_performace(model, loader):
-
-    model.to(device=DEVICE)
-    x, y = calculate_performance_of_model_on_data(model, loader)
-    # res["pKa_true"], res["pKa_pred"] = x, y
-    MAE = mean_absolute_error(x, y)
-    RMSE = np.sqrt(mean_squared_error(x, y))
-    R2 = r2_score(x, y)
-    # print(f"{dataset_name} - {model_name}: MAE {MAE}, RMSE {RMSE}")
-    # return pd.DataFrame(res)
-    return MAE, RMSE, R2
