95993aa417a5cc39d03e9f6b473fbc304074e7ea
Fritz Mayr <fmayr@a7srv2.pch.univie.ac.at>
2022-01-14 15:18:46 +0100

    docstrings in data.py completed

--- pkasolver/data.py
+++ pkasolver/data.py
@@ -23,7 +23,6 @@ from pkasolver.constants import (DEVICE, EDGE_FEATURES, NODE_FEATURES,
 
 
 def load_data(base: str = "data/Baltruschat") -> dict:
-
     """Helper function that takes path to working directory and
     returns a dictionary containing the paths to the training and testsets.
     
@@ -57,14 +56,13 @@ def import_sdf(sd_filename: str) -> pd.DataFrame:
     
     Parameters
     ----------
-    sdf_filename
+    sd_filename
         dataset path
     
     Returns
     ----------
-    dict
-        keys (str): dataset names  
-        values (str): dataset abs. paths
+    pd.DataFrame
+        DataFrame of dataset with Chem.rdchem.Mol object and molecule properties as columns
     
     """
 
@@ -75,18 +73,24 @@ def import_sdf(sd_filename: str) -> pd.DataFrame:
     return df
 
 
-def conjugates_to_dataframe(df: pd.DataFrame, mol_col: str = "ROMol") -> pd.DataFrame:
+def conjugates_to_dataframe(df: pd.DataFrame, mol_col: str = "ROMol", ph: float = 7.4) -> pd.DataFrame:
     """Takes DataFrame and returns a DataFrame with a column of calculated conjugated molecules.
+    Molecule properties must contain columns "marvin_atom" and "marvin_pka".
+
     Parameters
     ----------
     df
         DataFrame object
     mol_col
-        name of the column containing the Chem.rdchem.Mol objects
+        name of column containing the Chem.rdchem.Mol objects
+    ph
+        ph of the protonation state of the Chem.rdchem.Mol objects
+
     Returns
     -------
     pd.DataFrame
-        input dataframes with additional "Conjugates" column
+        df with additional "Conjugates" column
+
     """
     conjugates = []
     for i in tqdm.tqdm(range(len(df.index))):
@@ -94,7 +98,7 @@ def conjugates_to_dataframe(df: pd.DataFrame, mol_col: str = "ROMol") -> pd.Data
         index = int(df.marvin_atom[i])
         pka = float(df.marvin_pKa[i])
         try:
-            conj = create_conjugate(mol, index, pka, ignore_danger=True)
+            conj = create_conjugate(mol, index, pka, ignore_danger=True, pH=ph)
             conjugates.append(conj)
         except Exception as e:
             print(f"Could not create conjugate of mol number {i}")
@@ -107,17 +111,22 @@ def conjugates_to_dataframe(df: pd.DataFrame, mol_col: str = "ROMol") -> pd.Data
 def sort_conjugates(
     df: pd.DataFrame, mol_col_1: str = "ROMol", mol_col_2: str = "Conjugates"
 ) -> pd.DataFrame:
-    """Takes DataFrame and sorts the molecules in the two specified columns into two new columns, "protonated" and "deprotonated".
+    """Takes DataFrame, sorts the molecules in the two specified columns into 
+    two new columns, "protonated" and "deprotonated" that replace the two old columns.
+    Molecule properties must contain columns "marvin_atom".
+
     Parameters
     ----------
     df
         DataFrame object
     mol_col_1, mol_col_2
         name of the columns containing the Chem.rdchem.Mol objects
+
     Returns
     -------
     pd.DataFrame
-        input dataframes with the two columns specified, replaced by the columns "protonated" and "deprotonated"
+        df with the two columns specified, replaced by the columns "protonated" and "deprotonated"
+
     """
     prot = []
     deprot = []
@@ -148,31 +157,77 @@ def sort_conjugates(
 
 
 # data preprocessing functions - main
-def preprocess(sdf_filename: str) -> pd.DataFrame:
-    """Takes path of sdf file containing pkadata and returns a dataframe with column for protonated and deprotonated molecules.""" """Take name string and sdf path, process to Dataframe and save it as a pickle file."""
-    df = import_sdf(sdf_filename)
-    df = conjugates_to_dataframe(df)
+def preprocess(sd_filename: str, ph = 7.4) -> pd.DataFrame:
+    """Takes path of sdf file containing pkadata and returns a dataframe with column for protonated and deprotonated molecules.
+    Molecule properties must contain columns "marvin_atom" and "marvin_pka".
+    
+    Parameters
+    ----------
+    sd_filename 
+        dataset path
+    ph
+        ph of the protonation state of the Chem.rdchem.Mol objects
+
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame with molecule properties and "protonated" and "deprotonated" Chem.rdchem.Mol objects as columns
+    """ 
+    df = import_sdf(sd_filename)
+    df = conjugates_to_dataframe(df, ph=ph)
     df = sort_conjugates(df)
     df["pKa"] = df["pKa"].astype(float)
     return df
 
 
-def preprocess_all(sdf_files) -> dict:
+def preprocess_all(sd_files: dict, ph=7.4) -> dict:
     """Takes dictionary of pka data sets containing paths to sdf files, preprocesses them to Dataframes
     with protonated and deprotonated molecules and returns them in a dictionary.
+
+    Parameters
+    ----------
+    sd_files
+        keys (str): dataset name
+        values (str): dataset path
+
+    ph
+        ph of the protonation state of the Chem.rdchem.Mol objects
+
+    Returns
+    -------
+    dict
+        keys (str): dataset name
+        values (pd.DataFrame):  DataFrames with molecule properties and "protonated" 
+                                and "deprotonated" Chem.rdchem.Mol objects as columns
+
     """
     datasets = {}
-    for name, sdf_filename in sdf_files.items():
-        print(f"{name} : {sdf_filename}")
+    for name, sd_filename in sd_files.items():
+        print(f"{name} : {sd_filename}")
         print("###############")
-        datasets[name] = preprocess(sdf_filename)
+        datasets[name] = preprocess(sd_filename, ph=ph)
     return datasets
 
 
 # Neural net data functions - helpers
 class PairData(Data):
     """Extension of the Pytorch Geometric Data Class, which additionally
-    takes a conjugated molecules"""
+    takes a conjugated molecules.
+    
+    Attributes
+    -------
+    edge_index_p (torch.Tensor)
+        
+    x_p, x_d (torch.Tensor) 
+        Node feature matrix with shape [num_nodes, num_node_features]
+    edge_index_p, edge_index_d (torch.Tensor) 
+        Graph connectivity in COO format with shape [2, num_edges]
+    edge_attr_p, edge_attr_d (torch.Tensor) 
+        Edge feature matrix with shape [num_edges, num_edge_features]
+    charge_p, charge_d (int)
+        molecular charge 
+
+    """
 
     def __init__(
         self,
@@ -181,7 +236,7 @@ class PairData(Data):
         edge_attr_p,
         x_p,
         charge_p,
-        # everhtying for deprotonated
+        # everything for deprotonated
         edge_index_d,
         edge_attr_d,
         x_d,
@@ -215,7 +270,19 @@ from pandas.core.common import flatten
 
 
 def calculate_nr_of_features(feature_list: list) -> int:
-    """Calculates number of nodes and edge features from input list"""
+    """Calculates number of nodes and edge one hot features from input list.
+    
+    Parameters
+    ----------
+    features_list
+        list of features (must all be contained either in global dict node_feat_values or edge_feat_values)
+
+    Returns
+    -------
+    int
+        number of feature values
+
+    """
     i_n = 0
     if all(elem in node_feat_values for elem in feature_list):
         for feat in feature_list:
@@ -229,8 +296,9 @@ def calculate_nr_of_features(feature_list: list) -> int:
 
 
 def make_nodes(mol: Chem.rdchem.Mol, atom_idx: int, n_features: dict) -> torch.Tensor:
-    """Takes an rdkit.Mol, the atom index of the reaction center and a dictionary of node feature functions
+    """Takes an Chem.rdchem.Mol object, the atom index of the reaction center and a dictionary of node feature functions
     and returns a torch.tensor of node features for all atoms of one molecule.
+
     Parameters
     ----------
     mol
@@ -239,10 +307,12 @@ def make_nodes(mol: Chem.rdchem.Mol, atom_idx: int, n_features: dict) -> torch.T
         atom index of ionization center
     n_features
         dictionary containing functions for node feature generation
+
     Returns
     -------
     torch.Tensor
         tensor with dimensions num_nodes(atoms) x num_node_features.
+
     """
     x = []
     for atom in mol.GetAtoms():
@@ -257,20 +327,23 @@ def make_nodes(mol: Chem.rdchem.Mol, atom_idx: int, n_features: dict) -> torch.T
 def make_edges_and_attr(
     mol: Chem.rdchem.Mol, e_features
 ) -> Tuple[torch.Tensor, torch.Tensor]:
-    """Takes an rdkit.Mol and a dictionary of edge feature functions
+    """Takes an Chem.rdchem.Mol and a dictionary of edge feature functions
     and returns one tensor containing adjacency information and one of edge features for all edges of one molecule.
+
     Parameters
     ----------
     mol
         input molecule
     e_features
         dictionary containing functions for edge feature generation
+
     Returns
     -------
     edge_index
         tensor of dimension 2 x num_edges
     edge_attr
         tensor with dimensions num_edge) x num_edge_features.
+
     """
     edges = []
     edge_attr = []
@@ -289,17 +362,20 @@ def make_edges_and_attr(
 
 
 def make_features_dicts(all_features: dict, feat_list: list) -> dict:
-    """Take a dict of all features and a list of strings with all desired features
-    and return a dict with these features
+    """Takes a dict of all features and a list of strings with all desired features
+    and returns a dict with these features.
+
     ----------
     all_features
         dictionary of all available features and their possible values
     feat_list
         list of feature names to be filtered for
+
     Returns
     -------
     dict
         filtered features dictionary
+
     """
     return {x: all_features[x] for x in feat_list}
 
@@ -307,7 +383,8 @@ def make_features_dicts(all_features: dict, feat_list: list) -> dict:
 def mol_to_features(
     mol: Chem.rdchem.Mol, atom_idx: int, n_features: dict, e_features: dict
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, int]:
-    """Create the node and edge feature tensors from the input molecule
+    """Creates the node and edge feature tensors from the input molecule.
+
     Parameters
     ----------
     mol
@@ -318,6 +395,7 @@ def mol_to_features(
         dictionary containing functions for node feature generation
     e_features
         dictionary containing functions for node feature generation
+
     Returns
     -------
     nodes
@@ -328,6 +406,7 @@ def mol_to_features(
         tensor with dimensions num_edge) x num_edge_features.
     charge
         molecule charge
+
     """
     nodes = make_nodes(mol, atom_idx, n_features)
     edge_index, edge_attr = make_edges_and_attr(mol, e_features)
@@ -344,6 +423,8 @@ def mol_to_paired_mol_data(
 ) -> PairData:
     """Take a DataFrame row, a dict of node feature functions and a dict of edge feature functions
     and return a Pytorch PairData object.
+
+    Parameters
     ----------
     prot
         protonated rdkit mol object
@@ -355,10 +436,12 @@ def mol_to_paired_mol_data(
         dictionary of node features
     e_features
         dictionary of edge features
+
     Returns
     -------
     PairData
         Data object ready for use with Pytorch Geometric models
+
     """
     node_p, edge_index_p, edge_attr_p, charge_p = mol_to_features(
         prot, atom_idx, n_features, e_features
@@ -385,6 +468,8 @@ def mol_to_single_mol_data(
 ):
     """Take a DataFrame row, a dict of node feature functions and a dict of edge feature functions
     and return a Pytorch Data object.
+
+    Parameters
     ----------
     mol
         rdkit mol object
@@ -394,10 +479,12 @@ def mol_to_single_mol_data(
         dictionary of node features
     e_features
         dictionary of edge features
+
     Returns
     -------
     PairData
         Data object ready for use with Pytorch Geometric models
+
     """
     node_p, edge_index_p, edge_attr_p, charge = mol_to_features(
         mol, atom_idx, n_features, e_features
@@ -410,6 +497,8 @@ def make_pyg_dataset_from_dataframe(
 ) -> list:
     """Take a Dataframe, a list of strings of node features, a list of strings of edge features
     and return a List of PyG Data objects.
+
+    Parameters
     ----------
     df
         DataFrame containing "protonated" and "deprotonated" columns with mol objects, as well as "pKa" and "marvin_atom" columns
@@ -421,10 +510,12 @@ def make_pyg_dataset_from_dataframe(
         If true, including protonated and deprotonated molecules, if False only the type specified in mode
     mode
         if paired id false, use data from columnname == mol
+
     Returns
     -------
     list
         contains all molecules from df as pyG Graph data
+
     """
     print(f"Generating data with paired boolean set to: {paired}")
 
@@ -479,6 +570,8 @@ def make_paired_pyg_data_from_mol(
     mol: Chem.Mol, selected_node_features: dict, selected_edge_features: dict
 ) -> PairData:
     """Generate a PyG Data object from an input molecule and feature dictionaries.
+
+    Parameters
     ----------
     mol
         input molecule
@@ -486,10 +579,12 @@ def make_paired_pyg_data_from_mol(
         dictionary of selected node features
     selected_edge_features
         dictionary of selected edge features
+
     Returns
     -------
     PairData
         Data object ready for use with Pytorch Geometric models
+
     """
     props = mol.GetPropsAsDict()
     try:
@@ -537,7 +632,6 @@ def make_paired_pyg_data_from_mol(
         m.pka_type = props["marvin_pKa_type"]
     else:
         m.pka_type = ""
-
     try:
         m.ID = props["ID"]
     except:
@@ -546,7 +640,7 @@ def make_paired_pyg_data_from_mol(
 
 
 def iterate_over_acids(
-    acidic_mols_properties: Chem.Mol,
+    acidic_mols_properties: list,
     nr_of_mols: int,
     partner_mol: Chem.Mol,
     nr_of_skipped_mols: int,
@@ -555,8 +649,46 @@ def iterate_over_acids(
     pH: float,
     counter_list: list,
     smiles_list: list,
-):
+) -> Tuple[ 1, 2, 3, 4]:
+    """ Processes the acidic pKa values of an Schrödinger EPIK pka query
+    and returns a pair of protonated and deprotonated molecules for every 
+    pKa. Takes and updates global counters and skip trackers. 
 
+    Parameters
+    ----------
+    acidic_mols_properties
+        list of of dictionaries, each containing pka, ionization index 
+        and CHEMBL id of an acidic input molecule   
+    nr_of_mols
+        index number of molecule
+    partner_mol
+        molecule in protonation state at pH=pH
+    nr_of_skipped_mols
+        global number of skipped molecules
+    pka_list
+        list of all pKas already found for this molecule 
+    GLOBAL_COUNTER
+        counts total number of pKas processed
+    pH
+        pH of protonations state of partner_mol
+    counter_list: list,
+        list of values of GLOBAL_COUNTER
+    smiles_list: list,
+        list of smiles tuples of protonated and deprotonated molecules of all pka reactions
+
+    Returns
+    -------
+    acidic_mols (list) 
+        list of tuples of protonated and deprotonated molecules for 
+        every pka in acidic_mols_properties that did not yield an error  
+    nr_of_skipped_mols (int)
+        global number of skipped molecules
+    GLOBAL_COUNTER (int)
+        counts total number of pKas processed
+    skipping_acids
+        number of acids skipped (max 1)
+
+    """
     acidic_mols = []
     skipping_acids = 0
 
@@ -570,7 +702,6 @@ def iterate_over_acids(
                     partner_mol, acid_prop["atom_idx"], acid_prop["pka_value"], pH=pH,
                 )
                 Chem.SanitizeMol(new_mol)
-                # new_mol = s.standardize(new_mol)
 
             except Exception as e:
                 print(f"Error at molecule number {nr_of_mols} - acid enumeration")
@@ -624,7 +755,45 @@ def iterate_over_bases(
     counter_list: list,
     smiles_list: list,
 ):
+    """ Processes the basic pKa values of an Schrödinger EPIK pka query
+    and returns a pair of protonated and deprotonated molecules for every 
+    pKa. Takes and updates global counters and skip trackers. 
 
+    Parameters
+    ----------
+    basic_mols_properties
+        list of of dictionaries, each containing pka, ionization index 
+        and CHEMBL id of an basic input molecule   
+    nr_of_mols
+        index number of molecule
+    partner_mol
+        molecule in protonation state at pH=pH
+    nr_of_skipped_mols
+        global number of skipped molecules
+    pka_list
+        list of all pKas already found for this molecule 
+    GLOBAL_COUNTER
+        counts total number of pKas processed
+    pH
+        pH of protonations state of partner_mol
+    counter_list: list,
+        list of values of GLOBAL_COUNTER
+    smiles_list: list,
+        list of smiles tuples of protonated and deprotonated molecules of all pka reactions
+
+    Returns
+    -------
+    basic_mols (list) 
+        list of tuples of protonated and deprotonated molecules for 
+        every pka in basic_mols_properties that did not yield an error  
+    nr_of_skipped_mols (int)
+        global number of skipped molecules
+    GLOBAL_COUNTER (int)
+        counts total number of pKas processed
+    skipping_bases
+        number of bases skipped (max 1)
+
+    """
     basic_mols = []
     skipping_bases = 0
     for idx, basic_prop in enumerate(basic_mols_properties):
@@ -635,7 +804,6 @@ def iterate_over_bases(
                 )
 
                 Chem.SanitizeMol(new_mol)
-                # new_mol = s.standardize(new_mol)
 
             except Exception as e:
                 # in case error occurs new_mol is not in basic list
